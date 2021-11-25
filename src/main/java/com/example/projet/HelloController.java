39e3876ffbb0a05613c32395870d0aa8b5974c34
Killian Vendewinkele <killian.vendewinkele@gmail.com>
2021-11-25 10:12:06 +0100

    Start binary.

--- src/main/java/com/example/projet/HelloController.java
+++ src/main/java/com/example/projet/HelloController.java
@@ -1,14 +1,32 @@
 package com.example.projet;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+
+public abstract class HelloController implements Initializable {
+
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField decimal;
+
+    @FXML
+    private TextField binary;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        decimal.setOnAction(btnaction -> {
+            decimal.setText(String.valueOf(parseInt(decimal.getText())));
+        });
     }
 }

df04d210df1f6756cc5241f9c23e06841a7954cc
Killian Vendewinkele <killian.vendewinkele@gmail.com>
2021-11-25 09:13:33 +0100

    getter and setter of Book Class added.

--- src/main/java/com/example/projet/Book.java
+++ src/main/java/com/example/projet/Book.java
@@ -1,4 +1,58 @@
 package com.example.projet;
 
 public class Book {
+    private String name;
+    private String author;
+    private String resume;
+    private int column;
+    private int row;
+    private int publication;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getResume() {
+        return resume;
+    }
+
+    public void setResume(String resume) {
+        this.resume = resume;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getPublication() {
+        return publication;
+    }
+
+    public void setPublication(int publication) {
+        this.publication = publication;
+    }
 }
